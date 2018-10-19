7b9926eaebccad4267b2ea7230666f7ae69b2925
Andrew Oakes <andrew.oakes@wsu.edu>
2018-10-19 00:14:53 -0700

    Actually finished now

--- LUfact.c
+++ LUfact.c
@@ -36,7 +36,7 @@ void printAugMatrix(double** A, int N, short* order, double* x){
         for(int j = 0; j < N; j++){
             printf("%.5f ",A[order[i]][j]);
         }
-        printf("%f, ",x[i]);
+        printf("%.5f, ",x[i]);
         printf(" | ");
         printf("\n");
     }
@@ -50,7 +50,7 @@ LUfact *LUfactor(int N, const double **A) {
     LU->mutate = (short *) malloc(N*sizeof(short));
     double **B = LU->LU;
     float current, k, max = 0;
-    int maxIndex;
+    int maxIndex = 0;
     short save;
     // Clone A into LU
     for (int i = 0; i < N; i++){
@@ -61,15 +61,14 @@ LUfact *LUfactor(int N, const double **A) {
 
     for (int i = 0; i < N; i++)
         LU->mutate[i] = (short) i;
-    //printf("Initial Matrix \n");
-    printMatrix(B,N,LU->mutate);
     // actual factorizing goes here
     for(int i = 0; i < N-1; i++){ // Column index.
         // Find the maximum pivot in the current column.
         // Swap mutate i and mutate of max index so the pivot row is in the
         // ith index.
+        maxIndex = i;
         max = 0;
-        for(int m = 0; m < N; m++){
+        for(int m = i; m < N; m++){
             current = B[LU->mutate[m]][i];
             if(abs(current) > max){
                 max = current;
@@ -89,10 +88,6 @@ LUfact *LUfactor(int N, const double **A) {
             }
         }
     }
-    short a[] = {0,1,2,3,4};
-    printf("\n\n"); 
-    printMatrix(B,N,LU->mutate); 
-    printf("\n\n"); 
     return LU;
 }
 
@@ -106,11 +101,6 @@ void LUsolve(LUfact *fact, const double *b, double *x) {
     //Solve for LY = B (Lower)
     int N = fact->N;
     double y[N];
-    printf("B: ");
-    for(int n = 0; n < N; n++){
-        printf("%f, ",b[n]);
-    }
-    printf("\n");
     // Copy b into y.
     for(int n = 0; n < N; n++){
         y[n] = b[fact->mutate[n]];
@@ -120,29 +110,15 @@ void LUsolve(LUfact *fact, const double *b, double *x) {
             y[i] -= y[j]*fact->LU[fact->mutate[i]][j];
         }
     }
-    printf("Y:  ");
-    for(int l = 0; l < N; l++){
-        printf("%f,",y[l]);
-    }
-    printf("\n\n");
     // Copy Y into X
     for(int n = 0; n < N; n++){
         x[n] = y[n];
     }
     for(int i = N-1; i >= 0; i--){
-        printf("next = %f/%f \n",x[i],fact->LU[fact->mutate[i]][i]);
         x[i] /= fact->LU[fact->mutate[i]][i];
         for(int j = i-1; j >= 0; j--){
             x[j] -= x[i]*fact->LU[fact->mutate[j]][i];
-            printAugMatrix(fact->LU, fact->N, fact->mutate,x);
-            fact->LU[fact->mutate[j]][i] = 0;
         }
     }
-    printAugMatrix(fact->LU, fact->N, fact->mutate,x);
-    printf("X: ");
-    for(int n = 0; n < N; n++){
-        printf("%f, ",x[n]);
-    }
-    printf("\n\n");
-
+    //printAugMatrix(fact->LU, N, fact->mutate, x);
 }
